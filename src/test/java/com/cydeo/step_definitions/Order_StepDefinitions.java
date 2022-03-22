3fc1779526b0c47f24ebd834820f9afe2f2be293
JSedRay <jsedano85@gmail.com>
2022-03-22 14:10:47 -0400

    day.10 ViewAllOrdersPage.java

--- src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
@@ -2,6 +2,7 @@ package com.cydeo.step_definitions;
 
 import com.cydeo.pages.BasePage;
 import com.cydeo.pages.OrderPage;
+import com.cydeo.pages.ViewAllOrdersPage;
 import com.cydeo.pages.WebTableLoginPage;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.ConfigurationReader;
@@ -10,6 +11,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -21,6 +23,7 @@ public class Order_StepDefinitions {
     WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
     BasePage basePage = new BasePage();
     OrderPage orderPage = new OrderPage();
+    ViewAllOrdersPage viewAllOrdersPage = new ViewAllOrdersPage();
 
     @Given("user is already logged in and on order page")
     public void user_is_already_logged_in_and_on_order_page() {
@@ -34,6 +37,7 @@ public class Order_StepDefinitions {
         basePage.order.click();
 
     }
+
     @When("user selects product type {string}")
     public void user_selects_product_type(String string) {
 
@@ -41,6 +45,7 @@ public class Order_StepDefinitions {
         select.selectByVisibleText(string);
 
     }
+
     //    @When("user enters quantity {string}")
 //    public void user_enters_quantity(String string) {
 //
@@ -58,7 +63,7 @@ public class Order_StepDefinitions {
         //imitating pressing back_space button from keyboard to delete existing input
         orderPage.inputQuantity.sendKeys(Keys.BACK_SPACE);
 
-        orderPage.inputQuantity.sendKeys(quantity+"");
+        orderPage.inputQuantity.sendKeys(quantity + "");
 
     }
 
@@ -66,22 +71,27 @@ public class Order_StepDefinitions {
     public void user_enters_customer_name(String string) {
         orderPage.inputName.sendKeys(string);
     }
+
     @When("user enters street {string}")
     public void user_enters_street(String string) {
         orderPage.inputStreet.sendKeys(string);
     }
+
     @When("user enters city {string}")
     public void user_enters_city(String string) {
         orderPage.inputCity.sendKeys(string);
     }
+
     @When("user enters state {string}")
     public void user_enters_state(String string) {
         orderPage.inputState.sendKeys(string);
     }
+
     @When("user enters zipcode {string}")
     public void user_enters_zipcode(String string) {
         orderPage.inputZip.sendKeys(string);
     }
+
     @When("user selects credit card type {string}")
     public void user_selects_credit_card_type(String expectedCardType) {
 
@@ -90,23 +100,31 @@ public class Order_StepDefinitions {
 
 
     }
+
     @When("user enters credit card number {string}")
     public void user_enters_credit_card_number(String string) {
-
+        orderPage.cardNoInput.sendKeys(string);
     }
+
     @When("user enters expiry date {string}")
     public void user_enters_expiry_date(String string) {
-
+        orderPage.cardExpInput.sendKeys(string);
     }
+
     @When("user enters process order button")
     public void user_enters_process_order_button() {
-
+        orderPage.processOrderBtn.click();
     }
+
     @Then("user should see {string} in first row of the web table")
-    public void user_should_see_in_first_row_of_the_web_table(String string) {
+    public void user_should_see_in_first_row_of_the_web_table(String expectedName) {
+  //      System.out.println("viewAllOrdersPage.newCustomerCell.getText() = " + viewAllOrdersPage.newCustomerCell.getText());
+    String actualName = viewAllOrdersPage.newCustomerCell.getText();
+        Assert.assertEquals(expectedName, actualName);
+
+
 
     }
-
 
 
 }
